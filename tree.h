1672e1f6c772ca4ec5fbf2bde1408db09abe348e
Gabriel <129075181+eugabrielbr@users.noreply.github.com>
2023-04-25 20:23:38 -0300

    Update tree.h

--- tree.h
+++ tree.h
@@ -9,7 +9,8 @@ typedef struct Node{
     int fone;
     
     struct Node *left,*right;
-    
+    int height;
+
 } node;
 
 
@@ -19,10 +20,31 @@ node *create_node();
 
 node *insert_node(node *root, int cpf, char *name, char *birth, int fone);
 
-void fuction_print(node *root);
-
 node *delete(node *root, int cpf);
 
 node *search(node *root, int cpf);
+
+node *left_rotation(node *father);
+
+node *right_rotation(node *father);
+
+node *left_right_rotation(node *father);
+
+node *right_left_rotation(node *father);
+
+node *height(node *root,node *root2);
+
+node *balancing(node *root);
+
+int calculating_height(node *root);
+
+int whitch_is_bigger(int a, int b);
+
+int whitch_is_the_height(node *root);
+
+int fb(node *root);
+
+void fuction_print(node *root);
+
 
 #endif
